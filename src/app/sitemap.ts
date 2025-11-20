cef033d7e81f92e5b85e6050d09a299c13507a25
Air0flair <simon@airoflair.com>
2025-11-20 15:14:44 +0100

    Map

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,24 +1,24 @@
-// app/sitemap.ts
+import { MetadataRoute } from "next";
 
-export default function sitemap() {
+export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: 'https://airoflair.com',
+      url: "https://airoflair.com/",
       lastModified: new Date(),
-      changeFrequency: 'weekly',
+      changeFrequency: "weekly",
       priority: 1.0,
     },
     {
-      url: 'https://airoflair.com/privacy-policy',
+      url: "https://airoflair.com/privacy-policy",
       lastModified: new Date(),
-      changeFrequency: 'yearly',
+      changeFrequency: "yearly",
       priority: 0.4,
     },
     {
-      url: 'https://airoflair.com/terms-of-service',
+      url: "https://airoflair.com/terms-of-service",
       lastModified: new Date(),
-      changeFrequency: 'yearly',
+      changeFrequency: "yearly",
       priority: 0.4,
-    }
+    },
   ];
 }
